cede67ce650722f04808982b24b352967d850ef7
YamazakiYusuke <yusuke38098@gmail.com>
2023-12-29 07:00:06 +0900

    sameを削除

--- backend/src/domain/entities/utils/validate-props.ts
+++ backend/src/domain/entities/utils/validate-props.ts
@@ -1,13 +1,13 @@
-import { EntityCreationError } from "src/domain/errors/entity_error";
+import { EntityError } from "src/domain/errors/entity_error";
 import { Id } from "src/domain/values/id";
 
 export function validateProps<T extends object>(id: Id, props: T): void {
   if (!id) {
-    throw new EntityCreationError(`ID is required`);
+    throw new EntityError(`ID is required`);
   }
   Object.entries(props).forEach(([key, value]) => {
     if (!value) {
-      throw new EntityCreationError(`${key} is required`);
+      throw new EntityError(`${key} is required`);
     }
   });
 }
