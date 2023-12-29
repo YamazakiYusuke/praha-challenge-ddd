08b7aa0df0e30bc3648e0c43af39d0caf72e52d1
YamazakiYusuke <yusuke38098@gmail.com>
2023-12-29 16:09:10 +0900

    AAAパターンのコメントを追加

--- backend/src/domain/entities/__tests__/administrator.test.ts
+++ backend/src/domain/entities/__tests__/administrator.test.ts
@@ -6,47 +6,61 @@ import { Id } from "src/domain/values/id";
 describe('# Administrator Entity UnitTest\n', () => {
   describe('## create\n', () => {
     test('- Success create instance \n', () =>{
+      // 準備
       const email = Email.create('test@example.com') as Email;
       const props: AdministratorProps = { email };
+      // 実行
       const admin = Administrator.create(props);
+      // 確認
       expect(admin).toBeInstanceOf(Administrator);
     }); 
   });
 
   describe('## restore\n', () => {
     test('- Success create instance \n', () =>{
+      // 準備
       const id = Id.create();
       const email = Email.create('test@example.com') as Email;
       const props: AdministratorProps = { email };
+      // 実行
       const admin = Administrator.restore(id, props);
+      // 確認
       expect(admin).toBeInstanceOf(Administrator);
     }); 
   });
 
   describe('## email\n', () => {
     test('- Success to get instance \n', () =>{
+      // 準備
       const email = Email.create('test@example.com') as Email;
       const props: AdministratorProps = { email };
+      // 実行
       const admin = Administrator.create(props) as Administrator;
+      // 確認
       expect(admin.email).toEqual(email);
     }); 
   });
 
   describe('## changeEmail\n', () => {
     test('- Success to change value \n', () =>{
+      // 準備
       const email = Email.create('test@example.com') as Email;
       const newEmail = Email.create('newtest@example.com') as Email;
       const props: AdministratorProps = { email };
       const admin = Administrator.create(props) as Administrator;
+      // 実行
       admin.changeEmail(newEmail);
+      // 確認
       expect(admin.email).toEqual(newEmail);
     }); 
 
     test('- Failed to change value \n', () =>{
+      // 準備
       const email = Email.create('test@example.com') as Email;
       const newEmail = null as unknown as Email;
       const props: AdministratorProps = { email };
       const admin = Administrator.create(props) as Administrator;
+      // 実行・確認
       expect(() => admin.changeEmail(newEmail)).toThrow(EntityError);
     }); 
   });
