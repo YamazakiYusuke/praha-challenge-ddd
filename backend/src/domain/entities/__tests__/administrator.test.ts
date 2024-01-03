e66c351d750f15603a2782eb445b6644ef15627c
YamazakiYusuke <yusuke38098@gmail.com>
2024-01-03 19:06:58 +0900

    Administrator Entity UnitTestのリファクタリング

--- backend/src/domain/entities/__tests__/administrator.test.ts
+++ backend/src/domain/entities/__tests__/administrator.test.ts
@@ -1,27 +1,27 @@
 import { EntityError } from "src/domain/errors/entity_error";
-import { Administrator, AdministratorProps } from "../administrator";
 import { Email } from "src/domain/values/email";
 import { Id } from "src/domain/values/id";
+import { Administrator, AdministratorProps } from "../administrator";
 
 describe('# Administrator Entity UnitTest\n', () => {
   describe('## create\n', () => {
-    test('- Success create instance \n', () =>{
+    test('- Success create instance \n', () => {
       // 準備
-      const email = Email.create('test@example.com') as Email;
+      const email = Email.restore('test@example.com');
       const props: AdministratorProps = { email };
       // 実行
       const admin = Administrator.create(props) as Administrator;
       // 確認
       expect(admin).toBeInstanceOf(Administrator);
       expect(admin.email).toEqual(email);
-    }); 
+    });
   });
 
   describe('## restore\n', () => {
-    test('- Success create instance \n', () =>{
+    test('- Success create instance \n', () => {
       // 準備
-      const id = Id.create();
-      const email = Email.create('test@example.com') as Email;
+      const id = Id.restore('Id');
+      const email = Email.restore('test@example.com');
       const props: AdministratorProps = { email };
       // 実行
       const admin = Administrator.restore(id, props);
@@ -29,42 +29,33 @@ describe('# Administrator Entity UnitTest\n', () => {
       expect(admin).toBeInstanceOf(Administrator);
       expect(admin.getId).toEqual(id);
       expect(admin.email).toEqual(email);
-    }); 
+    });
   });
 
-  describe('## email\n', () => {
-    test('- Success to get instance \n', () =>{
-      // 準備
-      const email = Email.create('test@example.com') as Email;
-      const props: AdministratorProps = { email };
-      // 実行
-      const admin = Administrator.create(props) as Administrator;
-      // 確認
-      expect(admin.email).toEqual(email);
-    }); 
-  });
+  function getAdmin(): Administrator {
+    const id = Id.restore('Id');
+    const email = Email.restore('test@example.com');
+    const props: AdministratorProps = { email };
+    return Administrator.restore(id, props);
+  }
 
   describe('## changeEmail\n', () => {
-    test('- Success to change value \n', () =>{
+    test('- Success to change value \n', () => {
       // 準備
-      const email = Email.create('test@example.com') as Email;
-      const newEmail = Email.create('newtest@example.com') as Email;
-      const props: AdministratorProps = { email };
-      const admin = Administrator.create(props) as Administrator;
+      const admin = getAdmin();
+      const newEmail = Email.restore('newtest@example.com');
       // 実行
       admin.changeEmail(newEmail);
       // 確認
       expect(admin.email).toEqual(newEmail);
-    }); 
+    });
 
-    test('- Failed to change value \n', () =>{
+    test('- Failed to change value \n', () => {
       // 準備
-      const email = Email.create('test@example.com') as Email;
+      const admin = getAdmin();
       const newEmail = null as unknown as Email;
-      const props: AdministratorProps = { email };
-      const admin = Administrator.create(props) as Administrator;
       // 実行・確認
       expect(() => admin.changeEmail(newEmail)).toThrow(EntityError);
-    }); 
+    });
   });
 });
