697c646173632f5414c5817ff0d44b19d763328c
YamazakiYusuke <yusuke38098@gmail.com>
2024-01-10 07:07:22 +0900

    AssignmentProgressState UnitTest実装

---
+++ backend/src/domain/values/__tests__/assignment-progress-state.test.ts
@@ -0,0 +1,40 @@
+import { AssignmentProgressState } from "src/domain/values/assignment-progress-state";
+import { AssignmentProgressStateValue } from "src/util/enums";
+
+describe('# AssignmentProgressState UnitTest \n', () => {
+  describe('## create \n', () => {
+    it('- Success create instance \n', () => {
+      // 準備
+      const assignmentProgressState = AssignmentProgressState.create() as AssignmentProgressState;
+      // 実行・確認
+      expect(assignmentProgressState).toBeInstanceOf(AssignmentProgressState);
+      expect(assignmentProgressState.value).toEqual(AssignmentProgressStateValue.NotStarted.toString());
+    });
+  });
+
+  describe('## restore \n', () => {
+    it('- Success restore instance \n', () => {
+      // 準備
+      const assignmentProgressState = AssignmentProgressState.restore(AssignmentProgressStateValue.Completed.toString());
+      // 実行・確認
+      expect(assignmentProgressState).toBeInstanceOf(AssignmentProgressState);
+      expect(assignmentProgressState.value).toEqual(AssignmentProgressStateValue.Completed.toString());
+    });
+  });
+
+  describe('## isCompleted \n', () => {
+    it('- Check if completed \n', () => {
+      // 準備
+      const assignmentProgressState = AssignmentProgressState.restore(AssignmentProgressStateValue.Completed.toString());
+      // 実行・確認
+      expect(assignmentProgressState.isCompleted).toBe(true);
+    });
+
+    it('- Check if not completed \n', () => {
+      // 準備
+      const assignmentProgressState = AssignmentProgressState.restore(AssignmentProgressStateValue.NotStarted.toString());
+      // 実行・確認
+      expect(assignmentProgressState.isCompleted).toBe(false);
+    });
+  });
+});
