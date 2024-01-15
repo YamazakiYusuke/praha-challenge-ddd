ed2626122b687acddd1d764495c2ce3bf19a9972
YamazakiYusuke <yusuke38098@gmail.com>
2024-01-16 07:34:45 +0900

    GetAllParticipantsUsecaseの実装

---
+++ backend/src/app/participant/get-all-participants-usecase.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@nestjs/common";
+import { debuglog } from "util";
+import { ErrorResponse } from "../responses/error-response";
+import { ParticipantDTO } from "./dto/participant-dto";
+import { GetAllParticipantsQuery } from "src/domain/commands/participant/get-all-participant-query";
+import { Participant } from "src/domain/entities/participant";
+
+@Injectable()
+export class GetAllParticipantsUsecase {
+  constructor(
+    private readonly getAllParticipantsQuery: GetAllParticipantsQuery,
+  ) { }
+
+  public async execute(): Promise<ParticipantDTO[] | ErrorResponse> {
+    try {
+      const participants = await this.getAllParticipantsQuery.execute() as Participant[];
+      const participantDTOs = participants.map(e => new ParticipantDTO(e));
+      return participantDTOs;
+    } catch (e) {
+      debuglog(`Exception: ${e}`);
+      return new ErrorResponse('参加者の取得に失敗しました');
+    }
+  }
+}
