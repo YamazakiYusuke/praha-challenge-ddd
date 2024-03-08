e0ee5d6ac258a63ca6d0db71e494c607aef5a27a
YamazakiYusuke <yusuke38098@gmail.com>
2024-03-08 09:16:09 +0900

    CategoryChangeNameServiceリファクタリング　
    - domain-serviceでデータ永続化を行うように

--- backend/src/domain/services/__tests__/category/category-change-name-service.test.ts
+++ backend/src/domain/services/__tests__/category/category-change-name-service.test.ts
@@ -1,13 +1,15 @@
 import { GetCategoryByNameQuery } from 'src/domain/commands/category/get-one-category-by-name-query';
+import { SaveCategoryCommand } from 'src/domain/commands/category/save-category-command';
 import { Category, CategoryProps } from 'src/domain/entities/category';
 import { EntityError } from 'src/domain/errors/entity_error';
 import { CategoryChangeNameService } from 'src/domain/services/category/category-change-name-service';
-import { CategoryName } from 'src/domain/values/name';
 import { CategoryId } from 'src/domain/values/id';
-import { instance, mock, verify, when } from 'ts-mockito';
+import { CategoryName } from 'src/domain/values/name';
+import { anything, instance, mock, verify, when } from 'ts-mockito';
 
 describe('# CategoryChangeNameService UnitTest\n', () => {
   let getCategoryByNameQuery: GetCategoryByNameQuery;
+  let saveCategoryCommand: SaveCategoryCommand;
   let categoryChangeNameService: CategoryChangeNameService;
   const id = CategoryId.restore('Id');
   const name = CategoryName.restore('test');
@@ -17,9 +19,10 @@ describe('# CategoryChangeNameService UnitTest\n', () => {
 
   beforeEach(() => {
     getCategoryByNameQuery = mock(GetCategoryByNameQuery);
-    categoryChangeNameService = new CategoryChangeNameService(instance(getCategoryByNameQuery));
+    saveCategoryCommand = mock(SaveCategoryCommand);
+    categoryChangeNameService = new CategoryChangeNameService(instance(getCategoryByNameQuery), instance(saveCategoryCommand));
   });
-  
+
   describe('## execute\n', () => {
     test('- should change name of category\n', async () => {
       // 準備
@@ -37,6 +40,16 @@ describe('# CategoryChangeNameService UnitTest\n', () => {
       // 実行・確認
       await expect(categoryChangeNameService.execute(category, newName)).rejects.toThrow(EntityError);
       verify(getCategoryByNameQuery.execute(newName)).once();
+    });
+
+    test('should throw error if fail to save \n', async () => {
+      // 準備
+      when(getCategoryByNameQuery.execute(newName)).thenResolve(null);
+      when(saveCategoryCommand.execute(anything())).thenThrow(Error());
+      // 実行・確認
+      await expect(categoryChangeNameService.execute(category, newName)).rejects.toThrow(Error);
+      verify(getCategoryByNameQuery.execute(newName)).once();
+      verify(saveCategoryCommand.execute(anything())).once();
     });
   });
 });
--- backend/src/domain/services/category/category-change-name-service.ts
+++ backend/src/domain/services/category/category-change-name-service.ts
@@ -3,6 +3,7 @@ import { GetCategoryByNameQuery, IGetCategoryByNameQuery } from "src/domain/comm
 import { EntityError } from "src/domain/errors/entity_error";
 import { CategoryName } from "src/domain/values/name";
 import { Category } from "../../entities/category";
+import { SaveCategoryCommand } from "src/domain/commands/category/save-category-command";
 
 export interface ICategoryChangeNameService {
   execute(category: Category, newName: CategoryName): Promise<void | Error>;
@@ -12,7 +13,9 @@ export interface ICategoryChangeNameService {
 export class CategoryChangeNameService implements ICategoryChangeNameService {
   constructor(
     @Inject(GetCategoryByNameQuery) 
-    private readonly getCategoryByNameQuery: IGetCategoryByNameQuery
+    private readonly getCategoryByNameQuery: IGetCategoryByNameQuery,
+    @Inject(SaveCategoryCommand)
+    private readonly saveCategoryCommand: SaveCategoryCommand,
   ) { }
 
   async execute(category: Category, newName: CategoryName): Promise<void | Error> {
@@ -21,5 +24,6 @@ export class CategoryChangeNameService implements ICategoryChangeNameService {
       throw new EntityError('このカテゴリー名は既に存在しています');
     }
     category.changeName(newName);
+    await this.saveCategoryCommand.execute(category);
   }
 }
