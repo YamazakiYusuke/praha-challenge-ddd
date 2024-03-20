1a17f1c381346d990a5f68bed159ab1f07c9946f
YamazakiYusuke <yusuke38098@gmail.com>
2024-03-21 08:50:00 +0900

    PrismaCategoryRepositoryの実装

--- backend/src/infra/db/repositories/prisma/category-repository.ts
+++ backend/src/infra/db/repositories/prisma/category-repository.ts
@@ -1,16 +1,25 @@
-import { Prisma } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import { Category } from "src/domain/entities/category";
 import { ICategoryRepository } from "src/domain/repositories/category-repository";
+import { CategoryId } from "src/domain/values/id";
+import { CategoryName } from "src/domain/values/name";
 
 export class PrismaCategoryRepository implements ICategoryRepository {
-  private categories: Category[] = [];
-
+  private readonly prisma = new PrismaClient();
   async save(category: Category, tx?: Prisma.TransactionClient): Promise<void | Error> {
-    this.categories.push(category);
-    return;
+    const prismaClient = tx ?? this.prisma;
+    await prismaClient.category.upsert({
+      where: { id: category.id.value },
+      update: { name: category.name.value },
+      create: {
+        id: category.id.value,
+        name: category.name.value,
+      },
+    });
   }
-
+  
   async getAll(): Promise<Category[] | Error> {
-    return this.categories;
+    const categoriesData = await this.prisma.category.findMany();
+    return categoriesData.map(data => Category.restore(CategoryId.restore(data.id), { name: CategoryName.restore(data.name) }));
   }
 }
