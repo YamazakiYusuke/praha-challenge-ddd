9295476dfd2f459591a85fc48ea64d3706a4e99c
YamazakiYusuke <yusuke38098@gmail.com>
2024-03-20 22:56:03 +0900

    PrismaAdministratorRepositoryの実装

--- backend/src/infra/db/repositories/prisma/administrator-repository.ts
+++ backend/src/infra/db/repositories/prisma/administrator-repository.ts
@@ -1,16 +1,37 @@
-import { Prisma } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import { Administrator } from "src/domain/entities/administrator";
 import { IAdministratorRepository } from "src/domain/repositories/administrator-repository";
+import { Email } from "src/domain/values/email";
+import { AdministratorId } from "src/domain/values/id";
 
 export class PrismaAdministratorRepository implements IAdministratorRepository {
-  private administrators: Administrator[] = [];
+  private readonly prisma = new PrismaClient();
 
   async save(administrator: Administrator, tx?: Prisma.TransactionClient): Promise<void | Error> {
-    this.administrators.push(administrator);
-    return;
+    const prismaClient = tx ?? this.prisma;
+    await prismaClient.administrator.upsert({
+      where: {
+        id: administrator.id.value,
+      },
+      update: {
+        email: administrator.email.value,
+      },
+      create: {
+        id: administrator.id.value,
+        email: administrator.email.value,
+      }
+    });
   }
 
   async getAll(): Promise<Administrator[] | Error> {
-    return this.administrators;
+    const rowData = await this.prisma.administrator.findMany();
+    return rowData.map(data => {
+      return Administrator.restore(
+        AdministratorId.restore(data.id),
+        {
+          email: Email.restore(data.email),
+        }
+      );
+    });
   }
 }
