79edfff0b3b2e463996abcc036b2b3cc24a49d40
YamazakiYusuke <yusuke38098@gmail.com>
2024-03-22 08:03:40 +0900

    enumのrestore関数を作成

--- backend/src/util/enums.ts
+++ backend/src/util/enums.ts
@@ -16,3 +16,25 @@ export enum EmailStatus {
   Sent,
   Error,
 }
+
+// restore
+export function restoreEnrollmentStatusValue(value: number): EnrollmentStatusValue {
+  if (value in EnrollmentStatusValue) {
+    return value;
+  }
+  throw Error(`Unknown EnrollmentStatusValue: ${value}`);
+}
+
+export function restoreAssignmentProgressStateValue(value: number): AssignmentProgressStateValue {
+  if (value in AssignmentProgressStateValue) {
+    return value;
+  }
+  throw Error(`Unknown AssignmentProgressStateValue: ${value}`);
+}
+
+export function restoreEmailStatus(value: number): EmailStatus {
+  if (value in EmailStatus) {
+    return value;
+  }
+  throw Error(`Unknown EmailStatus: ${value}`);
+}
