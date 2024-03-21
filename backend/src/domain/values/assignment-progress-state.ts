10f008b0e0dbffe8db493e3d497a9857c2e61059
YamazakiYusuke <yusuke38098@gmail.com>
2024-03-22 08:15:29 +0900

    AssignmentProgressStateの修正

--- backend/src/domain/values/assignment-progress-state.ts
+++ backend/src/domain/values/assignment-progress-state.ts
@@ -1,25 +1,25 @@
 import { AssignmentProgressStateValue } from "src/util/enums";
 import { Value } from "./base/value";
 
-export class AssignmentProgressState extends Value<string> {
-  private constructor(value: string) {
+export class AssignmentProgressState extends Value<number> {
+  private constructor(value: number) {
     super(value)
   }
 
   static create(value: AssignmentProgressStateValue): AssignmentProgressState | Error {
-    return new AssignmentProgressState(value.toString());
-  }
-
-  static restore(value: string): AssignmentProgressState {
     return new AssignmentProgressState(value);
   }
 
-  public get value(): string {
+  static restore(value: AssignmentProgressStateValue): AssignmentProgressState {
+    return new AssignmentProgressState(value);
+  }
+
+  public get value(): number {
     return this.props
   }
 
   public get isCompleted() {
-    return this.props === AssignmentProgressStateValue.Completed.toString();
+    return this.props === AssignmentProgressStateValue.Completed;
   }
 }
 
