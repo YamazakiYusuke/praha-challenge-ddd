bdfe89669bf012d59c98688a9334933158e04f25
YamazakiYusuke <yusuke38098@gmail.com>
2024-03-21 09:18:46 +0900

    PrismaPairRepositoryの実装

--- backend/src/infra/db/repositories/prisma/pair-repository.ts
+++ backend/src/infra/db/repositories/prisma/pair-repository.ts
@@ -1,16 +1,57 @@
-import { Prisma } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import { Pair } from "src/domain/entities/pair";
 import { IPairRepository } from "src/domain/repositories/pair-repository";
+import { PairId, ParticipantId, TeamId } from "src/domain/values/id";
+import { PairName } from "src/domain/values/name";
 
 export class PrismaPairRepository implements IPairRepository {
-  private pairs: Pair[] = [];
+  private readonly prisma = new PrismaClient();
 
   async save(pair: Pair, tx?: Prisma.TransactionClient): Promise<void | Error> {
-    this.pairs.push(pair);
-    return;
+    const prismaClient = tx ?? this.prisma;
+    await prismaClient.pair.upsert({
+      where: { id: pair.id.value },
+      update: {
+        name: pair.name.value,
+        teamId: pair.teamId.value,
+        participants: {
+          connect: pair.participantIds.map(participantId => ({ id: participantId.value })),
+        },
+      },
+      create: {
+        id: PairId.create().value,
+        name: pair.name.value,
+        teamId: pair.teamId.value,
+        participants: {
+          connect: pair.participantIds.map(participantId => ({ id: participantId.value })),
+        },
+      },
+    });
   }
 
   async getAll(): Promise<Pair[] | Error> {
-    return this.pairs;
+    const pairsData = await this.prisma.pair.findMany({
+      select: {
+        id: true,
+        name: true,
+        teamId: true,
+        participants: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+
+    return pairsData.map(data => 
+      Pair.restore(
+        PairId.restore(data.id),
+        {
+          name: PairName.restore(data.name),
+          teamId: TeamId.restore(data.teamId),
+          participantIds: data.participants.map(participant => ParticipantId.restore(participant.id)),
+        }
+      )
+    );
   }
 }
