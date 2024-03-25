1e69bddfb7dcaf87f338be09ab3b28aa3de153ca
YamazakiYusuke <yusuke38098@gmail.com>
2024-03-25 09:13:52 +0900

    CreatePairService UnitTest修正

--- backend/src/domain/services/__tests__/pair/create-pair-service.test.ts
+++ backend/src/domain/services/__tests__/pair/create-pair-service.test.ts
@@ -1,7 +1,7 @@
 import { GetPairsByTeamIdQuery } from "src/domain/commands/pair/get-pairs-by-team-id-query";
 import { SavePairCommand } from "src/domain/commands/pair/save-pair-command";
 import { CreatePairService } from "src/domain/services/pair/create-pair-service";
-import { ParticipantId, ParticipantsId, TeamId } from "src/domain/values/id";
+import { ParticipantId, TeamId } from "src/domain/values/id";
 import { anything, instance, mock, verify, when } from 'ts-mockito';
 
 describe('# CreatePairService UnitTest\n', () => {
@@ -27,16 +27,16 @@ describe('# CreatePairService UnitTest\n', () => {
       verify(getPairsByTeamIdQuery.execute(teamId)).once();
       expect(result).toEqual(expect.objectContaining({
         _id: expect.anything(),
-        props: {
+        _props: {
           teamId: expect.objectContaining({
-            props: 'teamId'
+            _props: 'teamId'
           }),
           name: expect.objectContaining({
-            props: 'a'
+            _props: 'a'
           }),
           participantIds: [
-            { props: 'participantId1' },
-            { props: 'participantId2' }
+            { _props: 'participantId1' },
+            { _props: 'participantId2' }
           ]
         }
       }));
@@ -53,4 +53,5 @@ describe('# CreatePairService UnitTest\n', () => {
     });
   });
 });
+
 
