3cd902668e0754ed482d217d67d917c68fe5028f
YamazakiYusuke <yusuke38098@gmail.com>
2024-03-25 09:01:20 +0900

    Base Entity UnitTest 回収

--- backend/src/domain/entities/__tests__/base/entity.test.ts
+++ backend/src/domain/entities/__tests__/base/entity.test.ts
@@ -16,15 +16,40 @@ class SameEntity extends Entity<SameId, SameProps> {
   constructor(id: SameId, props: SameProps) {
     super(id, props);
   }
+  public get getForTestProps(): SameProps {
+    return this.props;
+  }
+
+  public setForTestProps(newProps: SameProps) {
+    return this.setProps(newProps);
+  }
 }
 
 describe('# Base Entity UnitTest\n', () => {
+  it('- should correctly set newProps \n', () => {
+    // 準備
+    const entity = new SameEntity(SameId.create(), { name: 'John', sex: 1 });
+    // 実行
+    const newProps = { name: 'Alex', sex: 2 }
+    entity.setForTestProps(newProps);
+    // 確認
+    expect(entity.getForTestProps).toEqual(newProps);
+  });
+
   it('- should correctly return the id\n', () => {
     // 準備
     const id = SameId.create();
     const entity = new SameEntity(id, { name: 'John', sex: 1 });
     // 実行・確認
     expect(entity.id).toEqual(id);
+  });
+
+  it('- should correctly return the props\n', () => {
+    // 準備
+    const props = { name: 'John', sex: 1 };
+    const entity = new SameEntity(SameId.create(), props);
+    // 実行・確認
+    expect(entity.getForTestProps).toEqual(props);
   });
 
   it('- should correctly compare two entities\n', () => {
