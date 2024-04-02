5dc006ea5ef68a094871cf88d6401829b0d153e3
YamazakiYusuke <yusuke38098@gmail.com>
2024-04-02 09:22:55 +0900

    EnrollParticipantService UnitTestの実装

--- backend/src/domain/services/__tests__/participant/enroll-participant-service.test.ts
+++ backend/src/domain/services/__tests__/participant/enroll-participant-service.test.ts
@@ -1,5 +1,6 @@
 import { GetPairWithFewestMembersQuery } from "src/domain/commands/pair/get-pair-with-fewest-members-query";
 import { SavePairCommand } from "src/domain/commands/pair/save-pair-command";
+import { GetParticipantByIdQuery } from "src/domain/commands/participant/get-participant-by-id-query";
 import { SaveParticipantCommand } from "src/domain/commands/participant/save-participant-command";
 import { Transaction } from "src/domain/commands/transaction/transaction";
 import { Pair } from "src/domain/entities/pair";
@@ -7,6 +8,7 @@ import { Participant, ParticipantProps } from "src/domain/entities/participant";
 import { EntityError } from "src/domain/errors/entity_error";
 import { CreateAdminEmailService } from "src/domain/services/admin_email/create-admin-email-service";
 import { SendAdminEmailService } from "src/domain/services/admin_email/send-admin-email-service";
+import { CreatePairService } from "src/domain/services/pair/create-pair-service";
 import { EnrollParticipantService } from "src/domain/services/participant/enroll-participant-service";
 import { Email } from "src/domain/values/email";
 import { PairId, ParticipantId, TeamId } from "src/domain/values/id";
@@ -16,15 +18,19 @@ import { anything, instance, mock, verify, when } from 'ts-mockito';
 
 describe('# EnrollParticipantService UnitTest\n', () => {
   let getPairWithFewestMembersQuery: GetPairWithFewestMembersQuery;
+  let createPairService: CreatePairService;
   let savePairCommand: SavePairCommand;
   let saveParticipantCommand: SaveParticipantCommand;
   let transaction: Transaction;
   let createAdminEmailService: CreateAdminEmailService;
   let sendAdminEmailService: SendAdminEmailService;
+  let getParticipantByIdQuery: GetParticipantByIdQuery;
   let enrollParticipantService: EnrollParticipantService;
 
   const teamId = TeamId.restore('teamId');
-  const pairId = PairId.restore('pairId');
+  function pairId(index: number) {
+    return PairId.restore(`pairId${index}`);
+  }
   function participantId(index: number) {
     return ParticipantId.restore(`participantId${index}`);
   }
@@ -41,18 +47,22 @@ describe('# EnrollParticipantService UnitTest\n', () => {
 
   beforeEach(() => {
     getPairWithFewestMembersQuery = mock(GetPairWithFewestMembersQuery);
+    createPairService = mock(CreatePairService);
     savePairCommand = mock(SavePairCommand);
     saveParticipantCommand = mock(SaveParticipantCommand);
     transaction = mock(Transaction);
     createAdminEmailService = mock(CreateAdminEmailService);
     sendAdminEmailService = mock(SendAdminEmailService);
+    getParticipantByIdQuery = mock(GetParticipantByIdQuery);
     enrollParticipantService = new EnrollParticipantService(
       instance(getPairWithFewestMembersQuery),
+      instance(createPairService),
       instance(savePairCommand),
       instance(saveParticipantCommand),
       instance(transaction),
       instance(createAdminEmailService),
       instance(sendAdminEmailService),
+      instance(getParticipantByIdQuery),
     );
   });
 
@@ -64,29 +74,61 @@ describe('# EnrollParticipantService UnitTest\n', () => {
       // 実行
       await expect(enrollParticipantService.execute(newParticipant)).rejects.toThrow(EntityError);
       // 確認
+      verify(getPairWithFewestMembersQuery.execute()).once();
+      verify(createPairService.execute(anything())).never();
+      verify(savePairCommand.execute(anything(), anything())).never();
+      verify(saveParticipantCommand.execute(anything(), anything())).never();
+      verify(transaction.execute(anything())).never();
       verify(createAdminEmailService.execute(anything())).once();
       verify(sendAdminEmailService.execute(anything())).once();
-      verify(saveParticipantCommand.execute(anything(), anything())).never();
-      verify(savePairCommand.execute(anything(), anything())).never();
-      verify(transaction.execute(anything())).never();
+      verify(getParticipantByIdQuery.execute(anything())).never();
     });
 
-    test('- when there is a pair with fewest members\n', async () => {
-      // 準備
-      const newParticipant = participant(participantId(1));
-      const smallestPair = Pair.restore(pairId, { teamId: teamId, name: PairName.restore('pairName'), participantIds: [participantId(2), participantId(3)] });
-      when(transaction.execute(anything())).thenCall(async (callback: (transaction: any) => Promise<void>) => {
-        await callback('');
+    describe('- when there is a pair with fewest members\n', () => {
+      test('- smallestPair.participantsLength == 3\n', async () => {
+        // 準備
+        const newParticipant = participant(participantId(1));
+        const smallestPair = Pair.restore(pairId(1), { teamId: teamId, name: PairName.restore('smallestPairName'), participantIds: [participantId(2), participantId(3), participantId(4)] });
+        const newPair = Pair.restore(pairId(2), { teamId: teamId, name: PairName.restore('newPairName'), participantIds: [participantId(4), participantId(1)] });
+        when(transaction.execute(anything())).thenCall(async (callback: (transaction: any) => Promise<void>) => {
+          await callback('');
+        });
+        when(getPairWithFewestMembersQuery.execute()).thenResolve(smallestPair);
+        when(getParticipantByIdQuery.execute(anything())).thenResolve(participant(participantId(4)))
+        when(createPairService.execute(anything())).thenResolve(newPair);
+        // 実行
+        await enrollParticipantService.execute(newParticipant);
+        // 確認
+        verify(getPairWithFewestMembersQuery.execute()).once();
+        verify(createPairService.execute(anything())).once();
+        verify(savePairCommand.execute(anything(), anything())).twice();
+        verify(saveParticipantCommand.execute(anything(), anything())).twice();
+        verify(transaction.execute(anything())).once();
+        verify(createAdminEmailService.execute(anything())).never();
+        verify(sendAdminEmailService.execute(anything())).never();
+        verify(getParticipantByIdQuery.execute(anything())).once();
       });
-      when(getPairWithFewestMembersQuery.execute()).thenResolve(smallestPair);
-      // 実行
-      await enrollParticipantService.execute(newParticipant);
-      // 確認
-      verify(saveParticipantCommand.execute(anything(), anything())).once();
-      verify(savePairCommand.execute(smallestPair, anything())).once();
-      verify(transaction.execute(anything())).once();
-      verify(createAdminEmailService.execute(anything())).never();
-      verify(sendAdminEmailService.execute(anything())).never();
+
+      test('- smallestPair.participantsLength != 3\n', async () => {
+        // 準備
+        const newParticipant = participant(participantId(1));
+        const smallestPair = Pair.restore(pairId(1), { teamId: teamId, name: PairName.restore('smallestPairName'), participantIds: [participantId(2), participantId(3)] });
+        when(transaction.execute(anything())).thenCall(async (callback: (transaction: any) => Promise<void>) => {
+          await callback('');
+        });
+        when(getPairWithFewestMembersQuery.execute()).thenResolve(smallestPair);
+        // 実行
+        await enrollParticipantService.execute(newParticipant);
+        // 確認
+        verify(getPairWithFewestMembersQuery.execute()).once();
+        verify(createPairService.execute(anything())).never();
+        verify(savePairCommand.execute(anything(), anything())).once();
+        verify(saveParticipantCommand.execute(anything(), anything())).once();
+        verify(transaction.execute(anything())).once();
+        verify(createAdminEmailService.execute(anything())).never();
+        verify(sendAdminEmailService.execute(anything())).never();
+        verify(getParticipantByIdQuery.execute(anything())).never();
+      });
     });
   });
 });
