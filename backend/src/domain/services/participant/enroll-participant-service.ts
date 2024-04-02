429192c2c05739559dc878ce62c93d5430faa392
YamazakiYusuke <yusuke38098@gmail.com>
2024-04-02 09:22:44 +0900

    EnrollParticipantServiceの修正

--- backend/src/domain/services/participant/enroll-participant-service.ts
+++ backend/src/domain/services/participant/enroll-participant-service.ts
@@ -1,11 +1,13 @@
 import { GetPairWithFewestMembersQuery } from "src/domain/commands/pair/get-pair-with-fewest-members-query";
 import { SavePairCommand } from "src/domain/commands/pair/save-pair-command";
+import { GetParticipantByIdQuery } from "src/domain/commands/participant/get-participant-by-id-query";
 import { SaveParticipantCommand } from "src/domain/commands/participant/save-participant-command";
 import { Transaction } from "src/domain/commands/transaction/transaction";
 import { Participant } from "src/domain/entities/participant";
 import { EntityError } from "src/domain/errors/entity_error";
 import { CreateAdminEmailService } from "src/domain/services/admin_email/create-admin-email-service";
 import { SendAdminEmailService } from "src/domain/services/admin_email/send-admin-email-service";
+import { CreatePairService } from "src/domain/services/pair/create-pair-service";
 import { AdminEmailContent } from "src/domain/values/admin-email-content";
 import { inject, injectable } from "tsyringe";
 
@@ -14,6 +16,8 @@ export class EnrollParticipantService {
   constructor(
     @inject(GetPairWithFewestMembersQuery)
     private readonly getPairWithFewestMembersQuery: GetPairWithFewestMembersQuery,
+    @inject(CreatePairService)
+    private readonly createPairService: CreatePairService,
     @inject(SavePairCommand)
     private readonly savePairCommand: SavePairCommand,
     @inject(SaveParticipantCommand)
@@ -24,6 +28,8 @@ export class EnrollParticipantService {
     private readonly createAdminEmailService: CreateAdminEmailService,
     @inject(SendAdminEmailService)
     private readonly sendAdminEmailService: SendAdminEmailService,
+    @inject(GetParticipantByIdQuery)
+    private readonly getParticipantByIdQuery: GetParticipantByIdQuery,
   ) { }
 
   async execute(participant: Participant): Promise<void> {
@@ -33,13 +39,30 @@ export class EnrollParticipantService {
       await this.sendAdminEmailService.execute(mail);
       throw new EntityError('参加可能なペアがありません');
     }
-    participant.changeEnrollmentStatusToEnrolled(smallestPair.teamId, smallestPair.id);
-    smallestPair.appendParticipant(participant.id);
+    if (smallestPair.participantsLength == 3) {
+      const moverId = smallestPair.lastParticipant;
+      const mover = await this.getParticipantByIdQuery.execute(moverId) as Participant;
 
-    await this.transaction.execute(async (tx) => {
-      await this.saveParticipantCommand.execute(participant, tx);
-      await this.savePairCommand.execute(smallestPair, tx);
-    })
+      smallestPair.removeParticipant(moverId);
+      const newPair = await this.createPairService.execute({ teamId: smallestPair.teamId, participantIds: [participant.id, moverId] });
+      participant.changeTeamIdPairId(newPair.id, newPair.teamId);
+      mover.changeTeamIdPairId(newPair.id, newPair.teamId);
 
+      await this.transaction.execute(async (tx) => {
+        await this.saveParticipantCommand.execute(participant, tx);
+        await this.saveParticipantCommand.execute(mover, tx);
+        await this.savePairCommand.execute(smallestPair, tx);
+        await this.savePairCommand.execute(newPair, tx);
+      })
+
+    } else {
+      participant.changeEnrollmentStatusToEnrolled(smallestPair.teamId, smallestPair.id);
+      smallestPair.appendParticipant(participant.id);
+
+      await this.transaction.execute(async (tx) => {
+        await this.saveParticipantCommand.execute(participant, tx);
+        await this.savePairCommand.execute(smallestPair, tx);
+      })
+    }
   }
 }
