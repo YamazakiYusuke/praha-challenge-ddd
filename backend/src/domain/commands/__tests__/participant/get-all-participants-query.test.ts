c74924c44ebc1f56befb6d137f680160f3e39acc
YamazakiYusuke <yusuke38098@gmail.com>
2024-04-04 08:41:04 +0900

    commandのUnitTestを実装

---
+++ backend/src/domain/commands/__tests__/participant/get-all-participants-query.test.ts
@@ -0,0 +1,56 @@
+import { GetAllParticipantsQuery } from 'src/domain/commands/participant/get-all-participants-query';
+import { Participant } from 'src/domain/entities/participant';
+import { IParticipantRepository } from 'src/domain/repositories/participant-repository';
+import { Email } from 'src/domain/values/email';
+import { PairId, TeamId } from 'src/domain/values/id';
+import { PersonName } from 'src/domain/values/name';
+import { EnrollmentStatusValue } from 'src/util/enums';
+import { instance, mock, verify, when } from 'ts-mockito';
+
+describe('# GetAllParticipantsQuery UnitTest \n', () => {
+  let query: GetAllParticipantsQuery;
+  let participantRepository: IParticipantRepository;
+
+  beforeEach(() => {
+    participantRepository = mock<IParticipantRepository>();
+    query = new GetAllParticipantsQuery(instance(participantRepository));
+  });
+
+  it('- should get all participants \n', async () => {
+    // 準備
+    const participant1 = Participant.create({
+      name: PersonName.create('Participant 1'),
+      email: Email.create('participant1@example.com'),
+      teamId: TeamId.create(),
+      pairId: PairId.create(),
+      enrollmentStatus: EnrollmentStatusValue.Enrolled
+    });
+    const participant2 = Participant.create({
+      name: PersonName.create('Participant 2'),
+      email: Email.create('participant2@example.com'),
+      teamId: TeamId.create(),
+      pairId: PairId.create(),
+      enrollmentStatus: EnrollmentStatusValue.Enrolled
+    });
+    when(participantRepository.getAll()).thenResolve([participant1, participant2]);
+
+    // 実行
+    const result = await query.execute();
+
+    // 確認
+    expect(result).toEqual([participant1, participant2]);
+    verify(participantRepository.getAll()).once();
+  });
+
+  it('- should get empty array when participantRepository.getAll returns empty array \n', async () => {
+    // 準備
+    when(participantRepository.getAll()).thenResolve([]);
+
+    // 実行
+    const result = await query.execute();
+
+    // 確認
+    expect(result).toEqual([]);
+    verify(participantRepository.getAll()).once();
+  });
+});
---
+++ backend/src/domain/commands/__tests__/participant/get-participant-by-team-id-query.test.ts
@@ -0,0 +1,57 @@
+import { GetParticipantByTeamIdQuery } from 'src/domain/commands/participant/get-participant-by-team-id-query';
+import { Participant } from 'src/domain/entities/participant';
+import { IParticipantRepository } from 'src/domain/repositories/participant-repository';
+import { Email } from 'src/domain/values/email';
+import { PairId, TeamId } from 'src/domain/values/id';
+import { PersonName } from 'src/domain/values/name';
+import { EnrollmentStatusValue } from 'src/util/enums';
+import { instance, mock, verify, when } from 'ts-mockito';
+
+describe('# GetParticipantByTeamIdQuery UnitTest \n', () => {
+  let query: GetParticipantByTeamIdQuery;
+  let participantRepository: IParticipantRepository;
+
+  beforeEach(() => {
+    participantRepository = mock<IParticipantRepository>();
+    query = new GetParticipantByTeamIdQuery(instance(participantRepository));
+  });
+
+  it('- should get participants by team id \n', async () => {
+    // 準備
+    const teamId = TeamId.create();
+    const participant1 = Participant.create({
+      name: PersonName.create('Participant 1'),
+      email: Email.create('participant1@example.com'),
+      teamId: teamId,
+      pairId: PairId.create(),
+      enrollmentStatus: EnrollmentStatusValue.Enrolled
+    });
+    const participant2 = Participant.create({
+      name: PersonName.create('Participant 2'),
+      email: Email.create('participant2@example.com'),
+      teamId: teamId,
+      pairId: PairId.create(),
+      enrollmentStatus: EnrollmentStatusValue.Enrolled
+    });
+    when(participantRepository.getAll()).thenResolve([participant1, participant2]);
+
+    // 実行
+    const result = await query.execute(teamId);
+
+    // 確認
+    expect(result).toEqual([participant1, participant2]);
+    verify(participantRepository.getAll()).once();
+  });
+
+  it('- should return null when participantRepository.getAll returns empty array \n', async () => {
+    // 準備
+    when(participantRepository.getAll()).thenResolve([]);
+
+    // 実行
+    const result = await query.execute(TeamId.create());
+
+    // 確認
+    expect(result).toEqual([]);
+    verify(participantRepository.getAll()).once();
+  });
+});
