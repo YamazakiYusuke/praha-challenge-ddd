653c832aec545f87dffe26ff505566c44dc0c23c
YamazakiYusuke <yusuke38098@gmail.com>
2024-04-16 09:11:20 +0900

    TeamControllerの実装

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,17 @@ import { Module } from '@nestjs/common';
 import { AssignmentProgressController } from 'src/controller/assignment-progress/assignment-progress.controller';
 import { PairController } from 'src/controller/pair/pair.controller';
 import { ParticipantController } from 'src/controller/participant/participant.controller';
+import { TeamController } from 'src/controller/team/team-controller';
 
 // memo: DIコンテナとしては使わないため、controllerの追加だけしてください
 @Module({
   imports: [],
-  controllers: [AssignmentProgressController, PairController, ParticipantController],
+  controllers: [
+    AssignmentProgressController, 
+    PairController, 
+    ParticipantController, 
+    TeamController,
+  ],
   providers: [],
 })
 export class AppModule { }
