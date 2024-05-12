a24b8c13c57e5a8d67e54688cac6eb190dffdd57
YamazakiYusuke <yusuke38098@gmail.com>
2024-05-12 15:05:39 +0900

    prismaのtransactionを修正

--- backend/src/infra/db/repositories/prisma/transaction-repository.ts
+++ backend/src/infra/db/repositories/prisma/transaction-repository.ts
@@ -5,7 +5,14 @@ export class PrismaTransactionRepository implements ITransactionRepository {
   private readonly prisma = new PrismaClient();
   async execute(callback: (transaction: Prisma.TransactionClient) => Promise<void>): Promise<void> {
     await this.prisma.$transaction(async (transaction) => {
-      return await callback(transaction);
+      // 外部キー制約を無効にする
+      await transaction.$executeRaw`SET session_replication_role = replica;`;
+      try {
+        return await callback(transaction);
+      } finally {
+        // 外部キー制約を有効に戻す
+        await transaction.$executeRaw`SET session_replication_role = DEFAULT;`;
+      }
     });
   }
 }
